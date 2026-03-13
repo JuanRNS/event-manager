10841eef4ff6c29487079c0381654e9a4146dce3
Juan <juanramalho90@gmail.com>
2026-03-13 17:09:41 -0300

    [fix] ajuste de env

--- event-manager-front-end/src/environments/environment.prod.ts
+++ event-manager-front-end/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     googleClientId: '540942343774-6ov02iakkgkm4rbjf8tec0igqs1u0scv.apps.googleusercontent.com',
-    api: 'https://event-manager-production-b7d9.up.railway.app/api/'
+    api: 'https://event-manager-production-bced.up.railway.app/api/'
 };
--- event-manager-front-end/src/environments/environment.ts
+++ event-manager-front-end/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     googleClientId: '540942343774-6ov02iakkgkm4rbjf8tec0igqs1u0scv.apps.googleusercontent.com',
-    api: 'http://localhost:8080/api/'
+    api: 'https://event-manager-production-bced.up.railway.app/api/'
 };
